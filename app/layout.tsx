464ab8ef4f76851173701197e442eea5308546f4
ksm0712 <karansm0712@gmail.com>
2026-06-09 12:17:34 +0800

    Add investment favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,9 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Investments",
   description: "Portfolio tracker",
+  icons: {
+    icon: "/icon.svg",
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
